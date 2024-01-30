581361c6a1aad4ff2a0db65d176a21ccaa5ccd28
alvadeli <alvadeli@mailbox.org>
2024-01-30 22:49:52 +0100

    replaced cs50 SQL with flask_sqlalchemy

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 
 # Configure application
 app = Flask(__name__)
@@ -9,9 +10,20 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///birthdays.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///birthdays_test.db" 
+db = SQLAlchemy(app)
 
+class birthday(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text, nullable=False)
+    month = db.Column(db.Integer, nullable=False)
+    day = db.Column(db.Integer, nullable=False)
+
+with app.app_context():    
+    db.create_all()
+
+# Configure CS50 Library to use SQLite database
+#db = SQL("sqlite:///birthdays.db")
 
 @app.after_request
 def after_request(response):
@@ -54,14 +66,17 @@ def index():
         if day < 0 or day > 31:
             return redirect("/")
 
-        db.execute("INSERT INTO birthdays(name,month,day) VALUES(?, ?, ?)", name, month, day)
+        #db.execute("INSERT INTO birthdays(name,month,day) VALUES(?, ?, ?)", name, month, day)
+        new_birthday = birthday(name=name, month=month, day=day)
+        db.session.add(new_birthday)
+        db.session.commit()
 
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
-        birthdays = db.execute("SELECT * FROM birthdays")
+        #birthdays = db.execute("SELECT * FROM birthdays")
+        birthdays = db.session.execute(db.select(birthday))
 
         return render_template("index.html", birthdays=birthdays)
-
